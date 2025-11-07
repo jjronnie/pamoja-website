ca5c4bec423d8393e01ee71571b9954742dbe5fd
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-11-08 00:41:39 +0300

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,9 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">   
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">  
-    @if (class_exists('SEO'))
-        {!! call_user_func(['SEO', 'generate']) !!}
-    @endif
+    {!! SEO::generate() !!}
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
